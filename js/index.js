6351bbf515aa81ba5bcb9bf426abb889cb63b916
Mykola Hamyliak <makolamak@gmail.com>
2020-02-17 05:38:04 +0000

    Second scratch of website

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
       $(".regular").slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         autoplay: true,
         autoplaySpeed: 5000,
+        mobileFirst: true,
       });
